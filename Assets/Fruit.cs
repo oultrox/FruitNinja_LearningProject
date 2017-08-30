90783d8cb9d6659c59c16f10698ecbdc8550403e
oultrox <jesus-perez95@hotmail.com>
2017-08-30 10:52:01 -0300

    [Upload] Main project with some functionalities already working
    - Blade fully working based on swipe min velocity
    - Fruit model is set
    - Fruit prefab can be sliced

---
+++ Assets/Fruit.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//Little fruit class that spawns it's sliced version.
+public class Fruit : MonoBehaviour {
+
+    [SerializeField] private GameObject fruitSlicedPrefab;
+    private Vector3 direction;
+    private Quaternion rotation;
+
+    //------API methods-----
+    //gets direction of the transform to the blade and calculates the angle.
+    private void OnTriggerEnter2D(Collider2D col)
+    {
+        if (col.CompareTag("Blade"))
+        {
+            
+            direction = (col.transform.position - transform.position).normalized; 
+            rotation = Quaternion.LookRotation(direction);
+            rotation.y = 0.7f; //just for cosmetic purposes
+            Instantiate(fruitSlicedPrefab,transform.position, rotation);
+            Destroy(gameObject);
+        }
+    }
+}
---
+++ Assets/Scripts/Blade.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//Blade component that follows the cursor.
+public class Blade : MonoBehaviour {
+
+    [SerializeField] GameObject bladeTrailPrefab;
+    [SerializeField] private float minCuttingVelocity = 0.015f;
+
+    private Vector2 previousPosition;
+    private Vector2 newPosition;
+    
+    private Rigidbody2D rbody;
+    private CircleCollider2D hitbox;
+
+    private Camera cam;
+    private GameObject currentBladeTrail;
+
+    private bool isCutting = false;
+    private float velocity;
+
+    //-------API methods--------
+    //Initialization
+    private void Start()
+    {
+        rbody = this.GetComponent<Rigidbody2D>();
+        cam = Camera.main;
+        hitbox = this.GetComponent<CircleCollider2D>();
+    }
+
+    // Update is called once per frame
+    void Update () {
+
+        if (Input.GetMouseButtonDown(0))
+        {
+            StartCutting();
+        }
+        else if (Input.GetMouseButtonUp(0))
+        {
+            StopCutting();
+        }
+
+        if (isCutting)
+        {
+            UpdateCut();
+        }
+	}
+    //Main method that calculates the velocity of our blades swipe movement
+    private void UpdateCut()
+    {
+        newPosition = cam.ScreenToWorldPoint(Input.mousePosition);
+        rbody.position = newPosition;
+
+        //Gets the velocity of our directional swipe.
+        velocity = (newPosition - previousPosition).magnitude / Time.deltaTime;
+
+        //if it's higher than the minimal, enable the collider in order to cut.
+        if (velocity > minCuttingVelocity)
+        {
+            hitbox.enabled = true;
+        }
+        else
+        {
+            hitbox.enabled = false;
+        }
+
+        //Updates the position
+        previousPosition = newPosition;
+    }
+
+    //Starts the cutting instantating the blade.
+    private void StartCutting()
+    {
+        isCutting = true;
+        //Always disabling the hitbox because update cut will check if we're slicing at the min speed.
+        hitbox.enabled = false;
+        currentBladeTrail = Instantiate(bladeTrailPrefab, transform);
+        newPosition = cam.ScreenToWorldPoint(Input.mousePosition);
+    }
+
+    //stops the cutting disabling and destroyinh the blade.
+    private void StopCutting()
+    {
+        isCutting = false;
+        hitbox.enabled = false;
+        currentBladeTrail.transform.SetParent(null);
+        Destroy(currentBladeTrail, 1f);
+    }
+
+    
+}
